03e60aa419b2ea2a5451fccb47b02ea22521315d
wley3337@gmail.com <wley3337@gmail.com>
2018-04-23 18:59:41 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma (words)
+def speak_to_grandma(words)
   #if words == "I LOVE YOU GRANDMA!"
     #"I LOVE YOU TOO PUMPKIN!"
   #elsif words == words.upcase
